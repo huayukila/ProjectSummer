da51cb6c360345ef9d8c9b0749f57f535b207ba0
puyoco <93511276+puyoco@users.noreply.github.com>
2023-09-10 18:10:47 +0900

    修改ScoreSystem为单例
    修改ScoreSystem为单例

--- Assets/Script/UI/ScoreSystem.cs
+++ Assets/Script/UI/ScoreSystem.cs
@@ -2,17 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ScoreSystem
+public class ScoreSystem:Singleton<ScoreSystem>
 {
     private int player1Score;
     private int player2Score;
-
-    public void InitializeScores()
+    void Start()
     {
         player1Score = 0;
         player2Score = 0;
     }
-
     public void AddScore(int playerNumber) //ScoreSystem.AddScore(1or2)でスコアを増加する。
     {
         if (playerNumber == 1)
