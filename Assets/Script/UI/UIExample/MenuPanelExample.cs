c3d70da82157dc759cbc652a1a41d955c74ca389
huayukila <66170588+huayukila@users.noreply.github.com>
2023-09-26 20:13:10 +0900

    更新了文件结构，添加ui制作样例

---
+++ Assets/Script/UI/UIExample/MenuPanelExample.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuPanelExample : MonoBehaviour
+{
+    //这里写获取的ui组件，绑定到panel上时记得把组件拖进去
+    //否则使用ui组件时会报空
+    public Button startBtn;
+    public Text showText;
+
+    private void Start()
+    {
+        startBtn.onClick.AddListener(() =>
+        {
+            //写业务逻辑，这里输出一个测试的log
+            Debug.Log("GameStart");
+        });
+        showText.text= string.Empty;
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space) )
+        {
+            showText.text = "hello";
+        }
+    }
+}
