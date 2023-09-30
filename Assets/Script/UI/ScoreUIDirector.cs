da06f8a9c311c0b78e8e616c60b0f0d61822003a
puyoco <93511276+puyoco@users.noreply.github.com>
2023-10-01 00:42:00 +0900

    复活倒数
    完成复活倒数（基本完成）

--- Assets/Script/UI/ScoreUIDirector.cs
+++ Assets/Script/UI/ScoreUIDirector.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.SceneManagement;
+using Unity.VisualScripting;
 
 public class ScoreUIDirector : MonoBehaviour
 {
@@ -10,6 +11,10 @@ public class ScoreUIDirector : MonoBehaviour
     public GameObject p2ScoreUI;
     public GameObject p1RespawnUI;
     public GameObject p2RespawnUI;
+    GameObject player1;
+    GameObject player2;
+    float player1Timer;
+    float player2Timer;
     public GameObject timeUI;
     public Timer timer;
     public float timerSetting = 60f;
@@ -17,33 +22,49 @@ public class ScoreUIDirector : MonoBehaviour
     void Start()
     {
         timer = new Timer();
-        timer.SetTimer(timerSetting, () => { Debug.Log("Timer finished!"); });
 
-        p1RespawnUI.SetActive(false);      //UIを隠す
+        player1Timer = Global.RESPAWN_TIME;
+        player2Timer = Global.RESPAWN_TIME;
+
+        timer.SetTimer(timerSetting, () => { Debug.Log("Timer finished!"); });      
+
+        p1RespawnUI.SetActive(false);      //復活のカウントダウンUIを隠す
         p2RespawnUI.SetActive(false);
+
+        this.player1 = GameObject.Find("Player1");
+        this.player2 = GameObject.Find("Player2");
     }
 
     // Update is called once per frame
     void Update()
     {
-        int scoreplayer01 = ScoreSystem.Instance.GetPlayer1Score();　　　　　　　//プレイヤ０１のスコアを代入
-        int scorePlayer02 = ScoreSystem.Instance.GetPlayer2Score();　　　　　　　//プレイヤ０２のスコアを代入
+        this.p1ScoreUI.GetComponent<TextMeshProUGUI>().text = "SCORE: " + ScoreSystem.Instance.GetPlayer1Score().ToString();　  //テキストの内容
+        this.p2ScoreUI.GetComponent<TextMeshProUGUI>().text = "SCORE: " + ScoreSystem.Instance.GetPlayer2Score().ToString();　  //テキストの内容
+        this.timeUI.GetComponent<TextMeshProUGUI>().text = "TIME:" + timer.GetTime().ToString("F2");     //タイマーのテキストの内容
+        
+        if (this.player1.active == false) 
+        {
+            p1RespawnUI.active= true;
+            player1Timer -= Time.deltaTime;
+            this.p1RespawnUI.GetComponent<TextMeshProUGUI>().text = player1Timer.ToString("F0");
+        }
+        else
+        {
+            p1RespawnUI.active = false;
+            player1Timer = Global.RESPAWN_TIME;
+        }
 
-        float timerRealTime = timer.GetTime();                          //タイマーのリアルタイムを代入
-
-        this.p1ScoreUI.GetComponent<TextMeshProUGUI>().text = "SCORE: " + scoreplayer01.ToString();　  //テキストの内容
-        this.p2ScoreUI.GetComponent<TextMeshProUGUI>().text = "SCORE: " + scorePlayer02.ToString();　  //テキストの内容
-        this.timeUI.GetComponent<TextMeshProUGUI>().text = "TIME:" + timerRealTime.ToString("F2");     //タイマーのテキストの内容
-
-        //if (!GameManager.Instance.playerOne.activeSelf)
-        //{
-
-        //}
-
-        //if (!GameManager.Instance.playerTwo.activeSelf)
-        //{
-
-        //}
+        if (this.player2.active == false)
+        {
+            p2RespawnUI.active = true;
+            player2Timer -= Time.deltaTime;
+            this.p2RespawnUI.GetComponent<TextMeshProUGUI>().text = player2Timer.ToString("F0");
+        }
+        else
+        {
+            p2RespawnUI.active = false;
+            player2Timer = Global.RESPAWN_TIME;
+        }
 
         if (timer.IsTimerFinished())                                    //タイマーが０になるとENDシーンに切り替える
         {
