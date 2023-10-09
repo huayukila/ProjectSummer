59671f382bb46a5004fb6352e18414fb6f508341
puyoco <93511276+puyoco@users.noreply.github.com>
2023-10-10 01:06:20 +0900

    End的UI构建
    End的UI构建（大概完成80%）

--- Assets/Script/System/UIsystem.cs
+++ Assets/Script/System/UIsystem.cs
@@ -1,8 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using Unity.Mathematics;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class UIsystem : MonoBehaviour
+public class UISystem : MonoBehaviour
 {
     public static void DisplayOn(GameObject ui)  //UIを表示
     {
@@ -22,12 +26,51 @@ public class UIsystem : MonoBehaviour
     {
 
     }
-    public static void MoveToLeft(GameObject ui)//UIを左に移動
+    public static void MoveToLeft(GameObject ui,float x,float speed)//UIを左に移動
     {
-
+        if (ui.transform.position.x >= x)
+        {
+            ui.transform.Translate(-speed, 0, 0);
+        }    
     }
     public static void TurnGray(GameObject ui)
     {
 
+    }
+    public static float GetPositionX(GameObject ui)
+    {
+        float x1 = ui.transform.position.x;
+        return x1;
+    }
+    public static float GetPositionY(GameObject ui)
+    {
+        float y1 = ui.transform.position.y;
+        return y1;
+    }
+    /// <summary>
+    /// ui現在の座標(x1,y1)に基づいてuiを新しい座標(x2,y2)に初期化する
+    /// </summary>
+    /// <param name="ui">GameObject</param>
+    /// <param name="x2">新しい座標x2</param>
+    /// <param name="y2">新しい座標y2</param>
+    public static void SetPos(GameObject ui, float x2, float y2)
+    {
+        ui.transform.position = new Vector3(GetPositionX(ui) + x2, GetPositionY(ui) + y2, 0f);
+    }
+    public static void SetLocalScale(GameObject ui,float x,float y,float z)
+    {
+        ui.transform.localScale=new Vector3(x,y,z);
+    }
+    public static void SetAlphaTMP(GameObject ui,float alpha)
+    {
+        Color colorUI = ui.GetComponent<TextMeshProUGUI>().color;
+        colorUI.a = alpha;
+        ui.GetComponent<TextMeshProUGUI>().color=colorUI;
+    }
+    public static void SetAlpha(GameObject ui, float alpha)
+    {
+        Color colorUI = ui.GetComponent<Image>().color;
+        colorUI.a = alpha;
+        ui.GetComponent<Image>().color = colorUI;
     }
 }
--- Assets/Script/UI/ScoreUIDirector.cs
+++ Assets/Script/UI/ScoreUIDirector.cs
@@ -9,16 +9,20 @@ public class ScoreUIDirector : MonoBehaviour
 {
     public GameObject p1ScoreUI;
     public GameObject p2ScoreUI;
+
     public GameObject p1RespawnUI;
     public GameObject p2RespawnUI;
-    GameObject player1;
-    GameObject player2;
+    //GameObject player1;//テスト用
+    //GameObject player2;
     float player1Timer;
     float player2Timer;
+
     public GameObject timeUI;
+
     public Timer timer;
+
     public float timerSetting = 60f;
-    // Start is called before the first frame update
+
     void Start()
     {
         timer = new Timer();
@@ -28,15 +32,16 @@ public class ScoreUIDirector : MonoBehaviour
 
         timer.SetTimer(timerSetting, () => { Debug.Log("Timer finished!"); });
 
-        UIsystem.DisplayOff(p1RespawnUI);//復活のカウントダウンUIを隠す
-        UIsystem.DisplayOff(p2RespawnUI);
-        this.player1 = GameObject.Find("Player1");
-        this.player2 = GameObject.Find("Player2");
+        UISystem.DisplayOff(p1RespawnUI);//復活のカウントダウンUIを隠す
+        UISystem.DisplayOff(p2RespawnUI);
 
-        TypeEventSystem.Instance.Register<Player1RespawnCntBegin>(e => { Player1RespawnCntBegin(); });
-        TypeEventSystem.Instance.Register<Player1RespawnCntEnd>(e => { Player1RespawnCntEnd(); });
-        TypeEventSystem.Instance.Register<Player2RespawnCntBegin>(e => { Player2RespawnCntBegin(); });
-        TypeEventSystem.Instance.Register<Player2RespawnCntEnd>(e => { Player2RespawnCntEnd(); });
+        //this.player1 = GameObject.Find("Player1");//テスト用
+        //this.player2 = GameObject.Find("Player2");
+        
+        TypeEventSystem.Instance.Register<Player1RespawnCntBegin>(e => { Player1RespawnCntBegin(); });  //プレイヤー１が死んだ時、カウントダウンを開始。
+        TypeEventSystem.Instance.Register<Player1RespawnCntEnd>(e =>   { Player1RespawnCntEnd(); });　　//プレイヤー１が復活する時、カウントダウンをリセット。
+        TypeEventSystem.Instance.Register<Player2RespawnCntBegin>(e => { Player2RespawnCntBegin(); });　//プレイヤー２が死んだ時、カウントダウンを開始。
+        TypeEventSystem.Instance.Register<Player2RespawnCntEnd>(e =>   { Player2RespawnCntEnd(); });　　//プレイヤー２が復活する時、カウントダウンをリセット。
     }
 
     // Update is called once per frame
@@ -46,6 +51,7 @@ public class ScoreUIDirector : MonoBehaviour
         this.p2ScoreUI.GetComponent<TextMeshProUGUI>().text = "SCORE: " + ScoreSystem.Instance.GetPlayer2Score().ToString();　  //テキストの内容
         this.timeUI.GetComponent<TextMeshProUGUI>().text = "TIME:" + timer.GetTime().ToString("F2");     //タイマーのテキストの内容
 
+        //テスト用‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐
         //if (this.player1.active ==false)
         //{
         //    TypeEventSystem.Instance.Send<Player1RespawnCntBegin>();
@@ -71,24 +77,24 @@ public class ScoreUIDirector : MonoBehaviour
     }
     void Player1RespawnCntBegin()
     {
-        UIsystem.DisplayOn(p1RespawnUI);         ////復活のカウントダウンUIを隠す
-        player1Timer -= Time.deltaTime;
-        p1RespawnUI.GetComponent<TextMeshProUGUI>().text = player1Timer.ToString("F1");
+        UISystem.DisplayOn(p1RespawnUI);         　　　　　　　　　　　　　　　　　　　　　//復活のカウントダウンUIを現す
+        player1Timer -= Time.deltaTime;                                                    //カウントダウンを開始
+        p1RespawnUI.GetComponent<TextMeshProUGUI>().text = player1Timer.ToString("F1");　　//カウントダウンのテキスト
     }
     void Player1RespawnCntEnd()
     {
-        UIsystem.DisplayOff(p1RespawnUI);
-        player1Timer = Global.RESPAWN_TIME;
+        UISystem.DisplayOff(p1RespawnUI);　　　　　　　　　　　　　　　　　　　　　　　　　//復活のカウントダウンUIを隠す
+        player1Timer = Global.RESPAWN_TIME;　　　　　　　　　　　　　　　　　　　　　　　　//カウントダウンをリセット
     }
     void Player2RespawnCntBegin()
     {
-        UIsystem.DisplayOn(p2RespawnUI);         ////復活のカウントダウンUIを隠す
-        player2Timer -= Time.deltaTime;
-        this.p2RespawnUI.GetComponent<TextMeshProUGUI>().text = player2Timer.ToString("F1");
+        UISystem.DisplayOn(p2RespawnUI);  　　　　　　　　　　　　　　　　　　　　　　　　 //復活のカウントダウンUIを現す       
+        player2Timer -= Time.deltaTime;                                                    //カウントダウンを開始
+        p2RespawnUI.GetComponent<TextMeshProUGUI>().text = player2Timer.ToString("F1");    //カウントダウンのテキスト
     }
     void Player2RespawnCntEnd()
     {
-        UIsystem.DisplayOff(p2RespawnUI);
-        player2Timer = Global.RESPAWN_TIME;
+        UISystem.DisplayOff(p2RespawnUI);                                                  //復活のカウントダウンUIを隠す
+        player2Timer = Global.RESPAWN_TIME;                                                //カウントダウンをリセット
     }
 }
