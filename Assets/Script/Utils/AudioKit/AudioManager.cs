c2eedbb048dd639bb1299ea4b50f4fefc8af720b
huayukila <66170588+huayukila@users.noreply.github.com>
2023-10-20 11:53:33 +0900

    重构播放器

--- Assets/Script/Utils/AudioKit/AudioManager.cs
+++ Assets/Script/Utils/AudioKit/AudioManager.cs
@@ -5,69 +5,69 @@ using System;
 interface AudioPlayer
 {
     void StopAllMusic();
+
     /// <summary>
     /// BGM再生
     /// </summary>
-    /// <param name="name">BGMの名前</param>
-    /// <param name="Volum">音量</param>
-    void PlayBGM(string name, float Volum);
-    void StopBGM();
+    /// <param name="audioName">BGMの名前</param>
+    /// <param name="volume">音量</param>
+    void PlayBGM(string audioName, float volume);
 
+    void StopBGM();
     void PauseBGM();
     void PauseAllMusic();
-
     void ContinueBGM();
+    void ContinueAllMusic();
 
-    void CountinueAllMusic();
     /// <summary>
     /// 普通でFX再生
     /// </summary>
-    /// <param name="name">名前</param>
-    /// <param name="Volum">音量</param>
-    void PlayFX(string name, float Volum);
+    /// <param name="audioName">名前</param>
+    /// <param name="volume">音量</param>
+    void PlayFX(string audioName, float volume);
+
     /// <summary>
     /// オブジェクトについていくのFX。まだ実装してないため、今は利用できません
     /// </summary>
-    /// <param name="name">名前</param>
-    /// <param name="Volum">音量</param>
-    /// <param name="Obj">オブジェクト</param>
+    /// <param name="audioName">名前</param>
+    /// <param name="volume">音量</param>
+    /// <param name="obj">オブジェクト</param>
     /// <param name="isLoop">繰り返して再生のか</param>
-    void PlayFX(string name, float Volum, GameObject Obj, bool isLoop);
+    void PlayFX(string audioName, float volume, GameObject obj, bool isLoop);
+
     /// <summary>
     /// 発生場所にそのまま残るために。同じ利用できません
     /// </summary>
-    /// <param name="name">名前</param>
-    /// <param name="Volum">音量</param>
-    /// <param name="transform">発生位置</param>
-    void PlayFX(string name, float Volum, Transform transform);
+    /// <param name="audioName">名前</param>
+    /// <param name="volume">音量</param>
+    /// <param name="targetTransform">発生位置</param>
+    void PlayFX(string audioName, float volume, Transform targetTransform);
 }
 
 
 public class AudioManager : Singleton<AudioManager>, AudioPlayer
 {
-    AudioSource BGMAudioSource;
-
-    Dictionary<string, AudioClip> FXM_audioDic;
-    Dictionary<string, AudioClip> BGM_AudioDic;
-
-    List<AudioSource> playingMusicList = new List<AudioSource>();
-
-    SimpleObjectPool<GameObject> FX_audioSourcePool;
-
+    AudioSource _bgmAudioSource;
+    Dictionary<string, AudioClip> _fxmAudioDic;
+    Dictionary<string, AudioClip> _bgmAudioDic;
+    readonly List<AudioSource> _playingMusicList=new List<AudioSource>();
+    SimpleObjectPool<GameObject> _fxAudioSourcePool;
 
     protected override void Awake()
     {
         base.Awake();
-        GameObject AudioPool=new GameObject("AudioPool");
-        DontDestroyOnLoad(AudioPool);
-        FX_audioSourcePool = new SimpleObjectPool<GameObject>(() =>
+        _fxAudioSourcePool = new SimpleObjectPool<GameObject>(() =>
         {
             GameObject obj = new GameObject();
-            obj.AddComponent<AudioSource>();
-            obj.AddComponent<FX_audioPlayer>();
-            obj.transform.SetParent(AudioPool.transform);
+            AudioSource audioSource = obj.AddComponent<AudioSource>();
+            obj.AddComponent<FXAudioPlayer>().Init(() =>
+            {
+                _fxAudioSourcePool.Recycle(obj);
+                _playingMusicList.Remove(audioSource);
+            });
+            obj.transform.SetParent(gameObject.transform);
             return obj;
-        }, (obj) =>
+        }, obj =>
         {
             AudioSource audioSource = obj.GetComponent<AudioSource>();
             audioSource.volume = 1.0f;
@@ -75,67 +75,57 @@ public class AudioManager : Singleton<AudioManager>, AudioPlayer
             audioSource.loop = false;
         }, 10);
         AudioDataBase audioDatabase = (AudioDataBase)Resources.Load("AudioDataBase");
-        BGM_AudioDic = audioDatabase.GetBGMAudioDic();
-        FXM_audioDic = audioDatabase.GetFXAudioDic();
-        BGMAudioSource = gameObject.AddComponent<AudioSource>();
-        BGMAudioSource.loop = true;
-    }
-
-    private void Update()
-    {
-        
+        _bgmAudioDic = audioDatabase.GetBGMAudioDic();
+        _fxmAudioDic = audioDatabase.GetFXAudioDic();
+        _bgmAudioSource = gameObject.AddComponent<AudioSource>();
+        _bgmAudioSource.loop = true;
     }
 
     public void StopAllMusic()
     {
         StopBGM();
-        foreach(var music in playingMusicList)
+        foreach (var music in _playingMusicList)
         {
             music.Pause();
         }
     }
 
-    public void PlayBGM(string name, float Volum)
+    public void PlayBGM(string audioName, float volume)
     {
-        BGMAudioSource.clip = BGM_AudioDic[name];
-        BGMAudioSource.volume = Volum;
-        BGMAudioSource.Play();
+        _bgmAudioSource.clip = _bgmAudioDic[audioName];
+        _bgmAudioSource.volume = volume;
+        _bgmAudioSource.Play();
     }
 
     public void StopBGM()
     {
-        BGMAudioSource?.Stop();
+        _bgmAudioSource?.Stop();
     }
 
-    public void PlayFX(string name, float Volum)
+    public void PlayFX(string audioName, float volume)
     {
-        GameObject fxObj = FX_audioSourcePool.Allocate();
-        AudioSource audioSource = fxObj.GetComponent<AudioSource>();
-        playingMusicList.Add(audioSource);
-        fxObj.GetComponent<FX_audioPlayer>().Init(FXM_audioDic[name], Volum, () =>
-        {
-            FX_audioSourcePool.Recycle(fxObj);
-            playingMusicList.Remove(audioSource);
-        });
+        GameObject fxObj = _fxAudioSourcePool.Allocate();
+        fxObj.GetComponent<FXAudioPlayer>().PlayMusic(_fxmAudioDic[audioName], volume);
+        _playingMusicList.Add(fxObj.GetComponent<AudioSource>());
     }
 
-    public void PlayFX(string name, float Volum, GameObject Obj, bool isLoop)
+    public void PlayFX(string audioName, float volume, GameObject obj, bool isLoop)
     {
     }
 
-    public void PlayFX(string name, float Volum, Transform transform)
+    public void PlayFX(string audioName, float volume, Transform targetTransform)
     {
     }
 
     public void PauseBGM()
     {
-        BGMAudioSource?.Pause();
+        _bgmAudioSource?.Pause();
     }
 
     public void PauseAllMusic()
     {
         PauseBGM();
-        foreach(var music in playingMusicList)
+        foreach (var music in _playingMusicList)
         {
             music.Pause();
         }
@@ -143,40 +133,48 @@ public class AudioManager : Singleton<AudioManager>, AudioPlayer
 
     public void ContinueBGM()
     {
-        BGMAudioSource?.UnPause();
+        _bgmAudioSource?.UnPause();
     }
 
-    public void CountinueAllMusic()
+    public void ContinueAllMusic()
     {
         ContinueBGM();
-        foreach (var music in playingMusicList)
+        foreach (var music in _playingMusicList)
         {
             music.UnPause();
         }
     }
 }
 
-public class FX_audioPlayer : MonoBehaviour
+public class FXAudioPlayer : MonoBehaviour
 {
-    AudioSource audioSource;
-    Action callBack;
+    AudioSource _audioSource;
+    Action _callBack;
+    bool _isPlaying = false;
+
+    private void Start()
+    {
+        _audioSource = GetComponent<AudioSource>();
+    }
+
     private void Update()
     {
-        if (audioSource != null)
-        {
-            if (!audioSource.isPlaying)
-            {
-                callBack?.Invoke();
-            }
-        }
+        if (_audioSource == null || !_isPlaying) return;
+        if (_audioSource.isPlaying) return;
+        _isPlaying = false;
+        _callBack?.Invoke();
     }
-    public void Init(AudioClip clip,float volum,Action callback)
-    {
-        audioSource = GetComponent<AudioSource>();
-        audioSource.clip = clip;
-        audioSource.volume = volum;
-        callBack = callback;
 
-        audioSource.Play();
+    public void PlayMusic(AudioClip clip, float volume)
+    {
+        _audioSource.clip = clip;
+        _audioSource.volume = volume;
+        _audioSource.Play();
+        _isPlaying = true;
+    }
+
+    public void Init(Action callback)
+    {
+        _callBack = callback;
     }
 }
