1636bd9769269cfe0db4e87e60f89507065724fe
puyoco <93511276+puyoco@users.noreply.github.com>
2023-12-07 01:26:02 +0900

    小地图更新
    还差蛛丝的出现拾取功能，以及地图的prefab化。

--- Assets/Script/UI/MiniMapController.cs
+++ Assets/Script/UI/MiniMapController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 
 public class MiniMapController : MonoBehaviour
@@ -8,20 +9,30 @@ public class MiniMapController : MonoBehaviour
     //ミニマップ関連のGameObject-----------
     public GameObject MiniMapSpider_Left;
     public GameObject MiniMapSpider_Right;
-    public GameObject MiniMapSilk;
-    Vector3 silkPos = new Vector3();
+
+    public GameObject MiniMapSilkPrefab;
+    private GameObject[] MiniMapSilkPrefabSave = new GameObject[3];
+
+    Vector3 silkPos = new Vector3(1500,1500,0);
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        for(int i = 0; i < 3; i++)
+        {
+            MiniMapSilkPrefabSave[i] = Instantiate(MiniMapSilkPrefab, new Vector3(2500,2500,0), Quaternion.identity);
+            MiniMapSilkPrefabSave[i].transform.SetParent(transform.parent);
+            MiniMapSilkPrefabSave[i].SetActive(false);
+        }
+        RawImage miniMapImage = GetComponent<RawImage>();
+        miniMapImage.texture = PolygonPaintManager.Instance.GetMiniMapRT();
         if (MiniMapSpider_Left != null)
         {
-            MiniMapSpider_Left.transform.position = GameManager.Instance.GetPlayerPos(1);
+            MiniMapSpider_Left.transform.position = GameManager.Instance.GetPlayerPos(1) + silkPos;
         }
         if (MiniMapSpider_Right != null)
         {
-            MiniMapSpider_Right.transform.position = GameManager.Instance.GetPlayerPos(2);
+            MiniMapSpider_Right.transform.position = GameManager.Instance.GetPlayerPos(2) + silkPos;
         }
     }
 
@@ -41,12 +52,21 @@ public class MiniMapController : MonoBehaviour
             Vector3 Spider_Right = GameManager.Instance.GetPlayerPos(2);
             MiniMapSpider_Right.transform.position = new Vector3(Spider_Right.x + transform.position.x, Spider_Right.z + transform.position.y, 0);
         }
-
-        foreach (Vector3 Pos in onFieldSilks)
+        if(onFieldSilks.Length != 0)
         {
-            silkPos.x=Pos.x;
-            silkPos.y=Pos.z;
-            Instantiate(MiniMapSilk, silkPos, Quaternion.identity);   
+            for (int i = 0; i < onFieldSilks.Length; i++) 
+            {
+                Vector3 tmp = onFieldSilks[i];
+                MiniMapSilkPrefabSave[i].transform.position = new Vector3(tmp.x + transform.position.x, tmp.z + transform.position.y, 0);
+                MiniMapSilkPrefabSave[i].SetActive(true);
+            }
         }
+    
+        //foreach (Vector3 Pos in onFieldSilks)
+        //{
+        //    silkPos.x = Pos.x;
+        //    silkPos.y = Pos.z;
+        //    Instantiate(MiniMapSilk, silkPos, Quaternion.identity);
+        //}
     }
 }
