cf040e554e3f2735e7a6f06f8ce3f42cdf733201
huayukila <66170588+huayukila@users.noreply.github.com>
2024-07-08 11:34:06 +0900

    UI部分实装

---
+++ Assets/Script/UI/Panel/TimeBarCtrl.Designer.cs
@@ -0,0 +1,5 @@
+using UnityEngine;
+public partial class TimeBarCtrl
+{
+	public UnityEngine.UI.Image front;
+}
---
+++ Assets/Script/UI/TimeBarCtrl.cs
@@ -0,0 +1,12 @@
+public partial class TimeBarCtrl : ViewController
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
